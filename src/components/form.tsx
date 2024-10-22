a56cc7b695bf331b5a514c8152e8fe7e7b4c6b3b
leonardofrp5 <leonardofrp5@gmail.com>
2024-10-21 20:28:43 -0500

    kick off

---
+++ src/components/form.tsx
@@ -0,0 +1,9 @@
+export default function Form() {
+  return (
+    <>
+      <span className="font-franklinBkcp">texto 1</span>
+      <span className="font-franklinDmcp">texto 2</span>
+      <span className="font-Peregrin">texto 3</span>
+    </>
+  );
+}
