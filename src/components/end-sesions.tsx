6585fcce055d0e3915264168c6609068645ad605
leonardofrp5 <leonardofrp5@gmail.com>
2024-10-21 21:13:25 -0500

    added section endsesions

---
+++ src/components/end-sesions.tsx
@@ -0,0 +1,18 @@
+export default function EndSesions() {
+  return (
+    <div className="bg-black flex flex-col items-center justify-center p-4">
+      <span className="font-franklinDmcp text-white text-base uppercase max-w-60 text-center">
+        Tus datos ya han sido enviados a tu contacto de emergencia
+      </span>
+      <span className="font-franklinDmcp text-white text-base uppercase my-5">
+        Corre seguro, corre con adidas
+      </span>
+      <button
+        type="submit"
+        className="flex justify-center pt-1 w-full font-franklinDmcp uppercase bg-white text-base text-black font-bold hover:bg-gray-300 transition"
+      >
+        Finaliza tu corrida
+      </button>
+    </div>
+  );
+}
