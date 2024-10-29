1eced93b3105e1ad67658ae6aedb347b674cd366
leonardofrp5 <leonardofrp5@gmail.com>
2024-10-28 20:36:45 -0500

    edit style un button end sesions

--- src/components/end-sesions.tsx
+++ src/components/end-sesions.tsx
@@ -1,39 +1,39 @@
-'use client'
+'use client';
 
-import { endSession } from '@/actions'
-import { useTransition } from 'react'
-import { toast } from 'react-toastify'
+import { endSession } from '@/actions';
+import { useTransition } from 'react';
+import { toast } from 'react-toastify';
 
 interface Props {
-  id: string
+  id: string;
 }
 
-export default function EndSesions ({ id }: Props) {
-  const [isPending, startTransition] = useTransition()
+export default function EndSesions({ id }: Props) {
+  const [isPending, startTransition] = useTransition();
 
   const handleFinish = () => {
     startTransition(async () => {
-      const { error } = await endSession(id)
-      if (error) toast.error(error)
-    })
-  }
+      const { error } = await endSession(id);
+      if (error) toast.error(error);
+    });
+  };
 
   return (
-    <div className='bg-black flex flex-col items-center justify-center p-4'>
-      <span className='font-franklinDmcp text-white text-base uppercase max-w-60 text-center'>
+    <div className="bg-black flex flex-col items-center justify-center p-4">
+      <span className="font-franklinDmcp text-white text-base uppercase max-w-60 text-center">
         Tus datos ya han sido enviados a tu contacto de emergencia
       </span>
-      <span className='font-franklinDmcp text-white text-base uppercase my-5'>
+      <span className="font-franklinDmcp text-white text-base uppercase my-5">
         Corre seguro, corre con adidas
       </span>
       <button
-        type='submit'
+        type="submit"
         disabled={isPending}
         onClick={handleFinish}
-        className='flex justify-center pt-1 w-full font-franklinDmcp uppercase bg-white text-base text-black font-bold hover:bg-gray-300 transition'
+        className="flex justify-center pt-1 w-full font-franklinDmcp uppercase bg-white text-base text-black font-bold hover:bg-gray-300 transition animate-ping-subtle-slow shadow-md shadow-white/50"
       >
         {isPending ? 'FINALIZANDO SESION  . . ' : 'FINALIZAR CORRIDA'}
       </button>
     </div>
-  )
+  );
 }
