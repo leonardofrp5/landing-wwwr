2bca3a118abf960defebf0f6c822c6f035d71758
leonardofrp5 <leonardofrp5@gmail.com>
2024-10-30 15:01:27 -0500

    update some svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
-import LogoAdidas from '../../public/logo-adidas.jpg'
 import Logo from '../../public/logo.svg'
+import LogoWwwr from '../../public/logo-wwwr.svg'
 import RegisterForm from '@/components/form'
 import { getActiveSession } from '@/actions'
 import EndSesions from '@/components/end-sesions'
@@ -11,13 +11,27 @@ const Home = async () => {
   return (
     <>
       <header className='flex flex-col justify-center items-center mb-4'>
-        <Image className='aspect-[30/20]' src={LogoAdidas} alt='With Women We Run' width={30} height={20} priority />
-        <Image src={Logo} className='aspect-square' alt='With Women We Run' width={200} height={200} priority />
+        <Image
+          src={LogoWwwr}
+          className='aspect-square'
+          alt='With Women We Run'
+          width={200}
+          height={200}
+          priority
+        />
       </header>
       {id ? <EndSesions id={id} /> : <RegisterForm />}
-      <div className='flex flex-col text-white text-center mt-8 h-full'>
+      <div className='flex flex-col text-white text-center mt-8 h-full items-center'>
         <span className='font-franklinDmcp'>LÍNEAS DE ATENCIÓN LOCAL:</span>
         <span className='font-franklinBkcp'>300 198 1392 - 678 78 8889</span>
+        <Image
+          src={Logo}
+          className='aspect-square mt-3'
+          alt='With Women We Run'
+          width={60}
+          height={60}
+          priority
+        />
       </div>
     </>
   )
